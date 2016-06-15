d6fef263fc96db767b580479d322cd003005aef0
sathya321 <ss1251989@gmail.com>
2016-06-15 18:58:26 +0530

    app commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GroupTableApp::Application.config.secret_token = '9e66565a05913a92ec79a76ffac481d4c8ff2c6749df4ca81054f4b2c1000a033875cd2f0f252581f0c0da92d5eeb1c8ecd0ac3ca285e334a30e3289e73d35bb'
